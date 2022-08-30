45d696ff217319536eaa58fe6abcd995a79cd70d
Andrea Bollini <andrea.bollini@4science.it>
2022-08-31 00:18:45 +0200

    Take special group in consideration for cris security

--- dspace-api/src/main/java/org/dspace/content/security/CrisSecurityServiceImpl.java
+++ dspace-api/src/main/java/org/dspace/content/security/CrisSecurityServiceImpl.java
@@ -96,6 +96,19 @@ public class CrisSecurityServiceImpl implements CrisSecurityService {
             return false;
         }
 
+        List<Group> specialGroups = context.getSpecialGroups();
+        if (CollectionUtils.isEmpty(specialGroups)) {
+            return false;
+        }
+
+        for (Group group : specialGroups) {
+            for (String groupMetadataField : groupMetadataFields) {
+                if (anyMetadataHasAuthorityEqualsTo(item, groupMetadataField, group.getID())) {
+                    return true;
+                }
+            }
+        }
+
         List<Group> userGroups = user.getGroups();
         if (CollectionUtils.isEmpty(userGroups)) {
             return false;
