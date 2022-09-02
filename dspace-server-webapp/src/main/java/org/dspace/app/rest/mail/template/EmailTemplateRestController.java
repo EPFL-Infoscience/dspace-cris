c4b7eaa393a093220922133fb3df6fb83e269312
Vincenzo Mecca <vincenzo.mecca@4science.com>
2022-09-02 17:38:25 +0200

    [CST-6640] Removed unused code

--- dspace-server-webapp/src/main/java/org/dspace/app/rest/mail/template/EmailTemplateRestController.java
+++ dspace-server-webapp/src/main/java/org/dspace/app/rest/mail/template/EmailTemplateRestController.java
@@ -21,12 +21,9 @@ import org.dspace.app.rest.utils.ContextUtil;
 import org.dspace.core.Context;
 import org.dspace.core.EmailTemplate;
 import org.dspace.core.I18nUtil;
-import org.dspace.services.ConfigurationService;
-import org.dspace.services.factory.DSpaceServicesFactory;
 import org.dspace.xmlworkflow.storedcomponents.ClaimedTask;
 import org.dspace.xmlworkflow.storedcomponents.service.ClaimedTaskService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -41,19 +38,6 @@ public class EmailTemplateRestController {
     protected static final String BASE_PATH = "api/email/template";
 
     private static final Logger log = LogManager.getLogger();
-
-    private static HttpHeaders initMap(ConfigurationService configService) {
-        HttpHeaders httpHeaders = new HttpHeaders();
-        httpHeaders.add(HttpHeaders.ACCESS_CONTROL_ALLOW_CREDENTIALS,
-                configService.getProperty("rest.cors.allow-credentials", "false"));
-        httpHeaders.add(HttpHeaders.ACCESS_CONTROL_ALLOW_ORIGIN,
-                configService.getProperty("rest.cors.allowed-origins"));
-        return httpHeaders;
-    }
-
-    ConfigurationService configurationService = DSpaceServicesFactory.getInstance().getConfigurationService();
-
-    HttpHeaders map = initMap(configurationService);
 
     @Autowired
     ClaimedTaskService claimedTaskService;
