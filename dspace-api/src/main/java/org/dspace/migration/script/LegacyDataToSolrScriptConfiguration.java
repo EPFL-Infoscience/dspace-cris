d759c124e69f8ba960f8e2c80a0642b1826210aa
corrado lombardi <corrado.lombardi@4science.com>
2023-03-31 10:01:28 +0200

    [CST-8717] Script to upload on a solr core the items to migrate gathered from s3

---
+++ dspace-api/src/main/java/org/dspace/migration/script/LegacyDataToSolrScriptConfiguration.java
@@ -0,0 +1,62 @@
+/*
+ * The contents of this file are subject to the license and copyright
+ * detailed in the LICENSE and NOTICE files at the root of the source
+ * tree and available online at
+ *
+ * http://www.dspace.org/license/
+ */
+
+package org.dspace.migration.script;
+
+import java.io.InputStream;
+import java.sql.SQLException;
+
+import org.apache.commons.cli.Options;
+import org.dspace.authorize.service.AuthorizeService;
+import org.dspace.core.Context;
+import org.dspace.scripts.configuration.ScriptConfiguration;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class LegacyDataToSolrScriptConfiguration<T extends LegacyDataToSolrScript> extends ScriptConfiguration<T> {
+    private Class<T> dspaceRunnableClass;
+
+    @Autowired
+    private AuthorizeService authorizeService;
+
+    @Override
+    public Class<T> getDspaceRunnableClass() {
+        return dspaceRunnableClass;
+    }
+
+    @Override
+    public void setDspaceRunnableClass(Class<T> dspaceRunnableClass) {
+        this.dspaceRunnableClass = dspaceRunnableClass;
+    }
+
+    @Override
+    public boolean isAllowedToExecute(Context context) {
+        try {
+            return authorizeService.isAdmin(context);
+        } catch (SQLException e) {
+            throw new RuntimeException("Unable to check if current user is admin", e);
+        }
+    }
+
+    @Override
+    public Options getOptions() {
+        if (options == null) {
+            Options options = new Options();
+
+            options.addOption("l", "limit", true, "maximum number of items to import");
+            options.getOption("l").setType(String.class);
+            options.getOption("l").setRequired(false);
+
+            options.addOption("f", "from", true, "start from file named");
+            options.getOption("f").setType(String.class);
+            options.getOption("f").setRequired(false);
+
+            super.options = options;
+        }
+        return options;
+    }
+}
