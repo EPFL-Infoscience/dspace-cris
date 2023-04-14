fb457de5902338cf3272be122608cb1546425f8d
Luca Giamminonni <luca.giamminonni@4science.it>
2023-04-14 12:29:38 +0200

    [CST-8705] Added log messages to verify post view call

--- dspace-api/src/main/java/org/dspace/statistics/SolrLoggerServiceImpl.java
+++ dspace-api/src/main/java/org/dspace/statistics/SolrLoggerServiceImpl.java
@@ -202,15 +202,21 @@ public class SolrLoggerServiceImpl implements SolrLoggerService, InitializingBea
     @Override
     public void postView(DSpaceObject dspaceObject, HttpServletRequest request,
                          EPerson currentUser) {
+
+        log.info("PostView called with request");
+
         if (solr == null || locationService == null) {
+            log.warn("SolrLoggerService - Solr = " + solr, " LocationService= " + locationService);
             return;
         }
+
         initSolrYearCores();
 
 
         try {
             SolrInputDocument doc1 = getCommonSolrDoc(dspaceObject, request, currentUser);
             if (doc1 == null) {
+                log.warn("SolrLoggerService - SolrInputDocument null");
                 return;
             }
             if (dspaceObject instanceof Bitstream) {
@@ -225,6 +231,9 @@ public class SolrLoggerServiceImpl implements SolrLoggerService, InitializingBea
 
 
             solr.add(doc1);
+
+            log.info("PostView - Document added");
+
             // commits are executed automatically using the solr autocommit
             boolean useAutoCommit = configurationService.getBooleanProperty("solr-statistics.autoCommit", true);
             if (!useAutoCommit) {
@@ -243,15 +252,21 @@ public class SolrLoggerServiceImpl implements SolrLoggerService, InitializingBea
     @Override
     public void postView(DSpaceObject dspaceObject,
                          String ip, String userAgent, String xforwardedfor, EPerson currentUser) {
+
+        log.info("PostView called");
+
         if (solr == null || locationService == null) {
+            log.warn("SolrLoggerService - Solr = " + solr, " LocationService= " + locationService);
             return;
         }
+
         initSolrYearCores();
 
         try {
             SolrInputDocument doc1 = getCommonSolrDoc(dspaceObject, ip, userAgent, xforwardedfor,
                                                       currentUser);
             if (doc1 == null) {
+                log.warn("SolrLoggerService - SolrInputDocument null");
                 return;
             }
             if (dspaceObject instanceof Bitstream) {
@@ -265,6 +280,9 @@ public class SolrLoggerServiceImpl implements SolrLoggerService, InitializingBea
             doc1.addField("statistics_type", StatisticsType.VIEW.text());
 
             solr.add(doc1);
+
+            log.info("PostView - Document added");
+
             // commits are executed automatically using the solr autocommit
             boolean useAutoCommit = configurationService.getBooleanProperty("solr-statistics.autoCommit", true);
             if (!useAutoCommit) {
