2d29b34e786bab1bde92fd5e474afa7686b1c76b
Luca Giamminonni <luca.giamminonni@4science.it>
2023-04-21 18:16:05 +0200

    [DSC-1059] Initialized BulkImportWorkbookBuilder

--- dspace-api/src/main/java/org/dspace/app/bulkimport/model/EntityRow.java
+++ dspace-api/src/main/java/org/dspace/app/bulkimport/model/EntityRow.java
@@ -8,12 +8,11 @@
 package org.dspace.app.bulkimport.model;
 
 import static java.util.Collections.unmodifiableList;
-import static org.apache.commons.collections4.multimap.UnmodifiableMultiValuedMap.unmodifiableMultiValuedMap;
 import static org.apache.commons.lang3.StringUtils.isBlank;
 
 import java.util.List;
 
-import org.apache.commons.collections4.MultiValuedMap;
+import org.apache.commons.collections4.ListValuedMap;
 import org.dspace.content.vo.MetadataValueVO;
 
 /**
@@ -34,14 +33,14 @@ public final class EntityRow {
 
     private final String submitter;
 
-    private final MultiValuedMap<String, MetadataValueVO> metadata;
+    private final ListValuedMap<String, MetadataValueVO> metadata;
 
     private final List<MetadataGroup> metadataGroups;
 
     private final List<UploadDetails> uploadDetails;
 
     public EntityRow(String id, String action, int row, Boolean discoverable, String submitter,
-        MultiValuedMap<String, MetadataValueVO> metadata, List<MetadataGroup> metadataGroups,
+        ListValuedMap<String, MetadataValueVO> metadata, List<MetadataGroup> metadataGroups,
         List<UploadDetails> uploadDetails) {
         this.id = id;
         this.submitter = submitter;
@@ -53,8 +52,12 @@ public final class EntityRow {
         this.uploadDetails = uploadDetails;
     }
 
-    public MultiValuedMap<String, MetadataValueVO> getMetadata() {
-        return unmodifiableMultiValuedMap(metadata);
+    public ListValuedMap<String, MetadataValueVO> getMetadata() {
+        return metadata;
+    }
+
+    public List<MetadataValueVO> getMetadata(String metadataField) {
+        return metadata.get(metadataField);
     }
 
     public List<MetadataGroup> getMetadataGroups() {
@@ -83,6 +86,13 @@ public final class EntityRow {
 
     public Boolean getDiscoverable() {
         return discoverable;
+    }
+
+    public String getDiscoverableValue() {
+        if (getDiscoverable() == null) {
+            return "";
+        }
+        return getDiscoverable() ? "Y" : "N";
     }
 
 }
