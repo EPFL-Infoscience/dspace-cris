2b77c61dbfe119625880fb67ec6b18a42788c838
corrado lombardi <corrado.lombardi@4science.com>
2023-06-27 20:34:57 +0200

    [CST-10609] deduplication notes visible

--- dspace-api/src/main/java/org/dspace/app/deduplication/utils/DedupUtils.java
+++ dspace-api/src/main/java/org/dspace/app/deduplication/utils/DedupUtils.java
@@ -18,6 +18,8 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.UUID;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import org.apache.commons.lang3.StringUtils;
 import org.apache.logging.log4j.LogManager;
@@ -354,7 +356,7 @@ public class DedupUtils {
 
         findDuplicateBySignature.addFilterQuery(filter);
 
-        findDuplicateBySignature.setFields("dedup.ids", "dedup.note", "dedup.flag");
+        findDuplicateBySignature.setFields("dedup.ids", "dedup.id", "dedup.note", "dedup.flag");
         findDuplicateBySignature.setRows(Integer.MAX_VALUE);
 
         if (configurationService.getBooleanProperty("deduplication.tool.duplicatechecker.ignorewithdrawn")) {
@@ -396,6 +398,22 @@ public class DedupUtils {
                             info.setDecision(DuplicateDecisionType.WORKFLOW, DuplicateDecisionValue.VERIFY);
                         } else if (SolrDedupServiceImpl.DeduplicationFlag.REJECTWF.getDescription().equals(flag)) {
                             info.setDecision(DuplicateDecisionType.WORKFLOW, DuplicateDecisionValue.REJECT);
+                        } else if (DeduplicationFlag.MATCH.getDescription().equals(flag)) {
+                            // FIXME: temporary solution to find previous decisions taken by other actors
+                            SolrQuery decisionsQuery = new SolrQuery();
+                            decisionsQuery.setQuery("dedup.id:" + solrDocument.getFieldValue("dedup.id"));
+                            decisionsQuery.setFields("dedup.note", "dedup.flag");
+                            decisionsQuery.setFilterQueries(Stream.of(DeduplicationFlag.VERIFYWS,
+                                                                      DeduplicationFlag.REJECTWS,
+                                                                      DeduplicationFlag.VERIFYWF,
+                                                                      DeduplicationFlag.REJECTWF)
+                                                                .map(DeduplicationFlag::getDescription)
+                                                                .map(v -> "dedup.flag: " + v)
+                                                                .collect(Collectors.joining(" OR ")));
+                            QueryResponse search = dedupService.search(decisionsQuery);
+                            search.getResults().forEach(doc -> {
+                                addDecisionsToInfo(info, doc);
+                            });
                         }
                         dupsInfo.add(info);
                         break;
@@ -406,6 +424,24 @@ public class DedupUtils {
 
         return dupsInfo;
 
+    }
+
+    private static void addDecisionsToInfo(DuplicateItemInfo info, SolrDocument doc) {
+        String f = (String) doc.getFieldValue("dedup.flag");
+        if (DeduplicationFlag.VERIFYWS.getDescription().equals(f)) {
+            info.setNote(DuplicateDecisionType.WORKSPACE,
+                         (String) doc.getFieldValue("dedup.note"));
+            info.setDecision(DuplicateDecisionType.WORKSPACE, DuplicateDecisionValue.VERIFY);
+        } else if (DeduplicationFlag.REJECTWS.getDescription().equals(f)) {
+            info.setDecision(DuplicateDecisionType.WORKSPACE, DuplicateDecisionValue.REJECT);
+        } else if (DeduplicationFlag.VERIFYWF.getDescription().equals(f)) {
+            info.setNote(DuplicateDecisionType.WORKFLOW,
+                         (String) doc.getFieldValue("dedup.note"));
+            info.setDecision(DuplicateDecisionType.WORKFLOW, DuplicateDecisionValue.VERIFY);
+        } else if (DeduplicationFlag.REJECTWF.getDescription().equals(f)) {
+            info.setDecision(DuplicateDecisionType.WORKFLOW,
+                             DuplicateDecisionValue.REJECT);
+        }
     }
 
     private boolean hasStoredDecision(UUID firstItemID, UUID secondItemID, DuplicateDecisionType decisionType)
