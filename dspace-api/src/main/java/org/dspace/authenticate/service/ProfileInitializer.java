55ebcb8f42c9f50bdeb34375d558ea54fad40f67
corrado lombardi <corrado.lombardi@4science.com>
2023-07-31 09:04:10 +0200

    updated group name

--- dspace-api/src/main/java/org/dspace/authenticate/service/ProfileInitializer.java
+++ dspace-api/src/main/java/org/dspace/authenticate/service/ProfileInitializer.java
@@ -64,7 +64,7 @@ public class ProfileInitializer {
         List.of("oairecerif.affiliation.role", "oairecerif.person.affiliation",
                 "oairecerif.affiliation.startDate", "oairecerif.affiliation.endDate");
     private final static Logger LOGGER = LoggerFactory.getLogger(ProfileInitializer.class);
-    private static final String SUBMITTERS = "Submitters";
+    private static final String SUBMITTERS = "Submitter";
 
     @Autowired
     private ResearcherProfileService researcherProfileService;
