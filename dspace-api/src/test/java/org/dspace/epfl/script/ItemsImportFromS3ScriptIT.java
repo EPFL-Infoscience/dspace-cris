bca27e88d4d44d13b9d273e6920f41aeb708ddca
Luca Giamminonni <luca.giamminonni@4science.it>
2023-09-27 11:24:03 +0200

    [CST-11876] Added title reader and fixed date issued format

--- dspace-api/src/test/java/org/dspace/epfl/script/ItemsImportFromS3ScriptIT.java
+++ dspace-api/src/test/java/org/dspace/epfl/script/ItemsImportFromS3ScriptIT.java
@@ -62,7 +62,7 @@ public class ItemsImportFromS3ScriptIT extends AbstractIntegrationTestWithDataba
 
         deleteAllFilesOnExit();
 
-        String[] args = new String[] { "items-import-from-s3", "-k", "55731.zip" };
+        String[] args = new String[] { "items-import-from-s3" };
 
         TestDSpaceRunnableHandler handler = new TestDSpaceRunnableHandler();
 
