729126d262c8431f9867c435f6e305edca1134b6
yevhenii.lohatskyi <lohatskyi.yevhenii@4science.com>
2023-10-04 19:28:54 +0300

    [CST-11890] update epfl-update-submitter script logic

--- dspace-api/src/main/java/org/dspace/epfl/script/SubmitterFixScript.java
+++ dspace-api/src/main/java/org/dspace/epfl/script/SubmitterFixScript.java
@@ -95,20 +95,57 @@ public class SubmitterFixScript
     }
 
     private void updateSubmitter(Item item) {
-        if (hasASciper(item.getSubmitter())) {
+        if (hasSciper(item.getSubmitter())) {
             handler.logInfo("Item " + item.getID() + " already has a submitter with sciper, not changed.");
             return;
         }
+
+        EPerson newSubmitter = getEPersonFromMetadata(item, "epfl.lastmodified.email");
+        if (hasSciper(newSubmitter)) {
+            updateSubmitter(item, newSubmitter);
+            return;
+        }
+
+        newSubmitter = getEPersonFromMetadata(item, "epfl.curator.email");
+        if (hasSciper(newSubmitter)) {
+            updateSubmitter(item, newSubmitter);
+            return;
+        }
+
         firstAuthorWithSciper(item)
-            .ifPresent(a -> {
-                handler.logInfo("Item " + item.getID() + " submitter updated with eperson: " + a.getEmail());
-                item.setSubmitter(a);
-                try {
-                    itemService.update(context, item);
-                } catch (SQLException | AuthorizeException e) {
-                    handler.handleException(e);
-                }
-            });
+            .ifPresentOrElse(
+                ePerson -> updateSubmitter(item, ePerson),
+                () -> updateSubmitter(item, StringUtils.isNotBlank(email) ? email : defaultEmail)
+            );
+    }
+
+    private void updateSubmitter(Item item, String email) {
+        try {
+            EPerson submitter = ePersonService.findByEmail(context, email);
+            if (submitter == null) {
+                handler.logInfo("Item " + item.getID() +
+                                ". No person found for email " + email + ", submitter not changed");
+                return;
+            }
+            updateSubmitter(item, submitter);
+        } catch (SQLException e) {
+            handler.handleException(e);
+        }
+    }
+
+    private void updateSubmitter(Item item, EPerson submitter) {
+        if (!StringUtils.equalsIgnoreCase(item.getSubmitter().getEmail(), submitter.getEmail())) {
+            handler.logInfo("Item " + item.getID() + " submitter updated from " + item.getSubmitter().getEmail() +
+                            " to " + submitter.getEmail());
+            item.setSubmitter(submitter);
+
+            try {
+                itemService.setMetadataSingleValue(context, item, "dc", "provenance", null, null, submitter.getEmail());
+                itemService.update(context, item);
+            } catch (SQLException | AuthorizeException e) {
+                handler.handleException(e);
+            }
+        }
     }
 
     private Optional<EPerson> firstAuthorWithSciper(Item item) {
@@ -138,6 +175,16 @@ public class SubmitterFixScript
         } catch (SQLException e) {
             handler.handleException(e);
             throw new RuntimeException(e);
+        }
+    }
+
+    private EPerson getEPersonFromMetadata(Item item, String metadata) {
+        String email = itemService.getMetadata(item, metadata);
+
+        try {
+            return ePersonService.findByEmail(context, email);
+        } catch (SQLException e) {
+            return null;
         }
     }
 
