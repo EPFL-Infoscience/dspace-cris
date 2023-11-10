5be8986ef01d65e53cd8501dd8dca28afe75e2f0
Luca Giamminonni <luca.giamminonni@4science.it>
2023-11-10 15:23:02 +0100

    [CST-12551] Refined rules for metadata related to containers

--- dspace-api/src/main/java/org/dspace/epfl/script/reader/ItemsImportSeriesReader.java
+++ dspace-api/src/main/java/org/dspace/epfl/script/reader/ItemsImportSeriesReader.java
@@ -8,6 +8,7 @@
 package org.dspace.epfl.script.reader;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -17,10 +18,17 @@ import javax.xml.xpath.XPathFactory;
 import org.apache.commons.lang.StringUtils;
 import org.dspace.content.dto.MetadataValueDTO;
 import org.dspace.core.Context;
+import org.dspace.services.ConfigurationService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 
 public class ItemsImportSeriesReader implements ItemsImportMetadataFieldReader {
+
+    @Autowired
+    private ConfigurationService configurationService;
+
+    private String isPartOfSeriesMetadataField;
 
     private XPath xPath = XPathFactory.newInstance().newXPath();
 
@@ -44,12 +52,22 @@ public class ItemsImportSeriesReader implements ItemsImportMetadataFieldReader {
                 .collect(Collectors.joining("; "));
 
             if (StringUtils.isNotBlank(value)) {
-                metadataValues.add(new MetadataValueDTO(metadataField, value));
+                String field = metadataField;
+                if (getIsPartOfSeriesTypes().contains(type)) {
+                    field = isPartOfSeriesMetadataField;
+                } else {
+                    System.out.println(type);
+                }
+                metadataValues.add(new MetadataValueDTO(field, value));
             }
 
         }
 
         return metadataValues;
+    }
+
+    private List<String> getIsPartOfSeriesTypes() {
+        return Arrays.asList(configurationService.getArrayProperty("epfl.items-import.is-part-of.series.types"));
     }
 
     @Override
@@ -71,6 +89,14 @@ public class ItemsImportSeriesReader implements ItemsImportMetadataFieldReader {
 
     public void setNumberXPath(String numberXPath) {
         this.numberXPath = numberXPath;
+    }
+
+    public String getIsPartOfSeriesMetadataField() {
+        return isPartOfSeriesMetadataField;
+    }
+
+    public void setIsPartOfSeriesMetadataField(String isPartOfSeriesMetadataField) {
+        this.isPartOfSeriesMetadataField = isPartOfSeriesMetadataField;
     }
 
 }
