050c31e563e8d6466f1ba6cee5247dbc406421bb
Luca Giamminonni <luca.giamminonni@4science.it>
2023-12-05 10:40:18 +0100

    [CST-12423] Added dispatcher for migration

--- dspace-api/src/main/java/org/dspace/epfl/script/ItemsImportFromS3Script.java
+++ dspace-api/src/main/java/org/dspace/epfl/script/ItemsImportFromS3Script.java
@@ -225,6 +225,7 @@ public class ItemsImportFromS3Script
             context = new Context(Mode.READ_ONLY);
         } else {
             context = new Context();
+            context.setDispatcher("epfl-migration");
         }
         assignCurrentUserInContext();
         assignSpecialGroupsInContext();
