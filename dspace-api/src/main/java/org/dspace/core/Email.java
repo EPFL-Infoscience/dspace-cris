f71f944445e896c64c9ec1b02d6d5ff300134404
yevhenii.lohatskyi <lohatskyi.yevhenii@4science.com>
2023-12-12 16:13:00 +0200

    [CST-12918] revert reqName and reqMessage to utf-8 in RequestItemEmailNotifier.sendRequest if email content is not html

--- dspace-api/src/main/java/org/dspace/core/Email.java
+++ dspace-api/src/main/java/org/dspace/core/Email.java
@@ -506,7 +506,7 @@ public class Email {
         }
     }
 
-    private boolean isHtmlContent() {
+    public boolean isHtmlContent() {
         return StringUtils.containsIgnoreCase(content, "<html>")
             && StringUtils.containsIgnoreCase(content, "</html>");
     }
