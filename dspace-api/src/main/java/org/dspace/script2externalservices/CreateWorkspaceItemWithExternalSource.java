6b4ff49eef4a65f9995610ef091e687f9fa8568b
yevhenii.lohatskyi <lohatskyi.yevhenii@4science.com>
2023-12-19 20:12:04 +0200

    [CST-12969] small fix

--- dspace-api/src/main/java/org/dspace/script2externalservices/CreateWorkspaceItemWithExternalSource.java
+++ dspace-api/src/main/java/org/dspace/script2externalservices/CreateWorkspaceItemWithExternalSource.java
@@ -650,7 +650,7 @@ public class CreateWorkspaceItemWithExternalSource extends DSpaceRunnable<
     }
 
     private Integer getDefaultPerResearcherSearchLimit() {
-        return configurationService.getIntProperty("importworkspaceitem.limit-total", Integer.MAX_VALUE);
+        return configurationService.getIntProperty("importworkspaceitem.limit-per-researcher", Integer.MAX_VALUE);
     }
 
     public Map<String, LiveImportDataProvider> getNameToProvider() {
