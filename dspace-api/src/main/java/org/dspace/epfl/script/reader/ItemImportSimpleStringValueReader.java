a22270e5d261f14492c3ad3433877999a070e16f
Daniele Ninfo <daniele.ninfo@4science.com>
2024-02-05 02:04:28 +0100

    javadoc for ItemImportSimpleStringValueReader

--- dspace-api/src/main/java/org/dspace/epfl/script/reader/ItemImportSimpleStringValueReader.java
+++ dspace-api/src/main/java/org/dspace/epfl/script/reader/ItemImportSimpleStringValueReader.java
@@ -16,7 +16,13 @@ import org.dspace.core.Context;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 
-
+/**
+ * Implementation of {@link ItemsImportMetadataFieldReader} that threats the value 
+ * of the metadata field as a simple String object
+ * 
+ * @author Aliaksei Bykau  
+ *
+ */
 public class ItemImportSimpleStringValueReader implements ItemsImportMetadataFieldReader {
 
     @Override
