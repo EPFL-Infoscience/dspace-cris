28ec7c59dd6f8596b6e832d66e7fbc323d3487fc
aliaksei.bykau <aliaksei.bykau@4science.com>
2024-02-07 16:07:13 +0100

    [CST-13563] added new ItemsImportUniqueMetadataReader and updated mapping for epfl.lastmodified.email metadata

--- dspace-api/src/test/java/org/dspace/epfl/script/reader/ItemImportReaderIT.java
+++ dspace-api/src/test/java/org/dspace/epfl/script/reader/ItemImportReaderIT.java
@@ -91,6 +91,30 @@ public class ItemImportReaderIT extends AbstractIntegrationTestWithDatabase {
                 firstTitle + delimiter + secondTitle + delimiter + subTitle);
     }
 
+    @Test
+    public void testUniqueMetadataReader() throws Exception {
+        String emailValue = "emailValue";
+        String test = " <record> \n" +
+                "<datafield tag=\"856\" ind1=\"0\" ind2=\" \">\n" +
+                "<subfield code=\"f\">" + emailValue + "</subfield>\n" +
+                "  </datafield>\n" +
+                "<datafield tag=\"856\" ind1=\"0\" ind2=\" \">\n" +
+                "<subfield code=\"f\">" + emailValue + "</subfield>\n" +
+                "  </datafield>\n" +
+                "</record>";
+        InputStream inputStream = new ByteArrayInputStream(test.getBytes());
+
+        Node record = marcXmlParser.parse(inputStream, mapping.getItemXPath());
+
+        List<MetadataValueDTO> itemMetadata = marcXmlParser.readItemMetadataValues(context, record, mapping);
+
+        int itemMetadataCount = (int) itemMetadata.stream()
+                .filter(metadataValueDTO -> metadataValueDTO.getMetadataField().equals("epfl.lastmodified.email"))
+                .count();
+
+        assertEquals(itemMetadataCount, 1);
+    }
+
 
     private String getFirstMetadataValue(List<MetadataValueDTO> metadata, String field) {
        return  metadata.stream()
