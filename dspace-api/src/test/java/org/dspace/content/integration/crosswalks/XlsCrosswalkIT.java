a42bf495daf7910ff3e299c22524cfb0f1353ae2
Daniele Ninfo <daniele.ninfo@4science.com>
2024-02-15 23:13:34 +0100

    [CST-12492] fixed a test

--- dspace-api/src/test/java/org/dspace/content/integration/crosswalks/XlsCrosswalkIT.java
+++ dspace-api/src/test/java/org/dspace/content/integration/crosswalks/XlsCrosswalkIT.java
@@ -380,16 +380,17 @@ public class XlsCrosswalkIT extends AbstractIntegrationTestWithDatabase {
         //assertThat(sheet.getPhysicalNumberOfRows(), equalTo(1)); // makes the test fail on purpose
         assertThat(sheet.getPhysicalNumberOfRows(), equalTo(2));
 
-        assertThat(getRowValues(sheet.getRow(0)), contains("Title", "Subtitle", "Type", "Language", "Publication date",
-            "Part of", "Journal or Serie", "ISBN (of the container)", "ISSN (of the container)",
-            "DOI (of the container)", "Publisher", "DOI", "ISBN", "ISSN", "ISI-Number", "SCP-Number", "Volume", "Issue",
-            "Start page", "End page", "Authors", "Editors", getTruncatedHeaderPrefix() + "Abstract",
-            "Event", "Product"));
+        assertThat(getRowValues(sheet.getRow(0)),
+                contains("Title", "Subtitle", "Type", "Language", "Publication date", "Part of", "Journal or Serie",
+                        "ISBN (of the container)", "ISSN (of the container)", "DOI (of the container)", "Publisher",
+                        "DOI", "ISBN", "ISSN", "ISI-Number", "SCP-Number", "Volume", "Issue", "Start page", "End page",
+                        "Authors", "Editors", getTruncatedHeaderPrefix() + "Abstract", "Event", "Product"));
 
-        assertThat(getRowValues(sheet.getRow(1)), contains("Test Publication", "Alternate publication title",
-            "http://purl.org/coar/resource_type/c_e059", "en", "2019-12-31", "", "", "", "", "", "",
-            "doi:222.222/publication", "", "", "", "", "V-02", "", "1", "20", "Edward Smith/Company||Walter White", "",
-            truncatedAbstract, "", ""));
+        assertThat(getRowValues(sheet.getRow(1)), contains("Test Publication", "Alternative publication title",
+                "http://purl.org/coar/resource_type/c_efa0", "en", "2020-01-01", "Published in publication", "", "", "",
+                "doi:10.3972/test", "Publication publisher", "doi:111.111/publication", "978-3-16-148410-0",
+                "2049-3630", "111-222-333", "99999999", "V.01", "Issue", "", "", "John Smith||Walter White/Company",
+                "Editor/Editor Affiliation", truncatedAbstract, "The best Conference", "DataSet"));
     }
 
     @Test
