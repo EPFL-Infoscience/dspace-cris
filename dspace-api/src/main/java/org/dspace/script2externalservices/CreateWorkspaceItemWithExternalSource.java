5ccabd362afbb31654c28e1d2305843a5f42917d
Julien Sicot <julien.sicot@epfl.ch>
2024-03-08 22:38:11 +0100

    EPFL Deduplication for input from external sources should preferably utilize DOI identifiers

--- dspace-api/src/main/java/org/dspace/script2externalservices/CreateWorkspaceItemWithExternalSource.java
+++ dspace-api/src/main/java/org/dspace/script2externalservices/CreateWorkspaceItemWithExternalSource.java
@@ -528,10 +528,10 @@ public class CreateWorkspaceItemWithExternalSource extends DSpaceRunnable<
 
         switch (service) {
             case SCOPUS:
-                metadata.setQualifier(SCOPUS);
+                metadata.setQualifier("doi");
                 break;
             case WOS:
-                metadata.setQualifier("isi");
+                metadata.setQualifier("doi");
                 break;
             case CROSSREF:
                 metadata.setQualifier("doi");
