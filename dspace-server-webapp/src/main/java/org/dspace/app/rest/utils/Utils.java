5bf4ba9cb87980d126d4a17c754a6e80f8fd3947
Giuseppe Digilio <giuseppe.digilio@4science.com>
2024-08-07 17:07:51 +0200

    [CST-16076] Add logs to debug error

--- dspace-server-webapp/src/main/java/org/dspace/app/rest/utils/Utils.java
+++ dspace-server-webapp/src/main/java/org/dspace/app/rest/utils/Utils.java
@@ -1054,7 +1054,7 @@ public class Utils {
             log.warn("urlIsPrefixOf(dspaceUrl, uri) = {}", urlIsPrefixOf(dspaceUrl, uri));
             log.warn("StringUtils.isBlank(dspaceSSRUrl) = {}", StringUtils.isBlank(dspaceSSRUrl));
             log.warn("urlIsPrefixOf(dspaceSSRUrl, uri) = {}", urlIsPrefixOf(dspaceSSRUrl, uri));
-            log.warn("dspaceUrl = {}", dspaceSSRUrl);
+            log.warn("dspaceUrl = {}", dspaceUrl);
             log.warn("dspaceSSRUrl = {}", dspaceSSRUrl);
             log.warn("uri = {}", uri);
             throw new IllegalArgumentException("the supplied uri is not ours: " + uri);
