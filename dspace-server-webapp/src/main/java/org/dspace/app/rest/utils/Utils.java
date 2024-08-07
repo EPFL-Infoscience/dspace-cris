61f0c9497d1fff627cb54b00ddeb787dbe974d40
Giuseppe Digilio <giuseppe.digilio@4science.com>
2024-08-07 17:58:15 +0200

    [CST-16076] Remove log messages

--- dspace-server-webapp/src/main/java/org/dspace/app/rest/utils/Utils.java
+++ dspace-server-webapp/src/main/java/org/dspace/app/rest/utils/Utils.java
@@ -1050,13 +1050,6 @@ public class Utils {
         // Check whether the URI could be valid.
         if (!urlIsPrefixOf(dspaceUrl, uri) && (StringUtils.isBlank(dspaceSSRUrl) ||
             !urlIsPrefixOf(dspaceSSRUrl, uri))) {
-
-            log.warn("urlIsPrefixOf(dspaceUrl, uri) = {}", urlIsPrefixOf(dspaceUrl, uri));
-            log.warn("StringUtils.isBlank(dspaceSSRUrl) = {}", StringUtils.isBlank(dspaceSSRUrl));
-            log.warn("urlIsPrefixOf(dspaceSSRUrl, uri) = {}", urlIsPrefixOf(dspaceSSRUrl, uri));
-            log.warn("dspaceUrl = {}", dspaceUrl);
-            log.warn("dspaceSSRUrl = {}", dspaceSSRUrl);
-            log.warn("uri = {}", uri);
             throw new IllegalArgumentException("the supplied uri is not ours: " + uri);
         }
 
