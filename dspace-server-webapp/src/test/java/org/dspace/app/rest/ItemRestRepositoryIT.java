3c2a99efef0a9a6e249df65c5fd982b2df922b48
Andrea Bollini <andrea.bollini@4science.com>
2024-10-12 18:16:21 +0200

    CST-16918 avoid exception if the query is empty

--- dspace-server-webapp/src/test/java/org/dspace/app/rest/ItemRestRepositoryIT.java
+++ dspace-server-webapp/src/test/java/org/dspace/app/rest/ItemRestRepositoryIT.java
@@ -5645,6 +5645,10 @@ public class ItemRestRepositoryIT extends AbstractControllerIntegrationTest {
                 .param("q", "http://example.com/sample"))
                 .andExpect(status().isNoContent());
 
+        getClient(token).perform(get("/api/core/items/search/findByCustomURL")
+                .param("q", ""))
+                .andExpect(status().isNoContent());
+
     }
 
     @Test
