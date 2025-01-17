d85ef075283ddccda3a3e074925f5a8fd460ba28
Daniele Ninfo <daniele.ninfo@4science.com>
2025-01-17 01:25:14 +0100

    CST-18712 reverted test to the expected status

--- dspace-server-webapp/src/test/java/org/dspace/app/rest/DiscoveryRestControllerIT.java
+++ dspace-server-webapp/src/test/java/org/dspace/app/rest/DiscoveryRestControllerIT.java
@@ -770,7 +770,7 @@ public class DiscoveryRestControllerIT extends AbstractControllerIntegrationTest
                 .andExpect(status().isUnprocessableEntity());
 
         getClient().perform(get("/api/discover/facets/author_editor").param("query", "title:"))
-                .andExpect(status().isBadRequest());
+                .andExpect(status().isUnprocessableEntity());
     }
 
     @Test
