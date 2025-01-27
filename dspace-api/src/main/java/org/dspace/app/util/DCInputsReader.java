93754bf6f8411677a54f1599cb177c3809504845
Adamo <adamo.fapohunda@4science.com>
2025-01-27 10:31:36 +0100

    [CST-12441] Updated DCInputsReader to use a composite key for attribute
    (cherry picked from commit 8dbe263ca25b1e4f982462f87912811368d00c23)

--- dspace-api/src/main/java/org/dspace/app/util/DCInputsReader.java
+++ dspace-api/src/main/java/org/dspace/app/util/DCInputsReader.java
@@ -539,7 +539,7 @@ public class DCInputsReader {
                                                        "." + field.get("dc-qualifier") +
                                                        " has no language attribute");
                         } else {
-                            field.put(PAIR_TYPE_NAME, pairTypeName);
+                            field.put("language." + PAIR_TYPE_NAME, pairTypeName);
                         }
                     }
                 } else if (StringUtils.equalsIgnoreCase(tagName, "linked-metadata-field")) {
@@ -598,7 +598,7 @@ public class DCInputsReader {
                                            "." + field.get("dc-qualifier") +
                                            " has no name attribute");
             } else {
-                field.put(PAIR_TYPE_NAME, pairTypeName);
+                field.put(value + "." + PAIR_TYPE_NAME, pairTypeName);
             }
         }
     }
@@ -739,7 +739,7 @@ public class DCInputsReader {
                         || type.equals("qualdrop_value")
                         || type.equals("openlist")
                         || type.equals("list"))) {
-                        String pairsName = fld.get(PAIR_TYPE_NAME);
+                        String pairsName = fld.get(type + "." + PAIR_TYPE_NAME);
                         List<String> v = valuePairs.get(pairsName);
                         if (v == null) {
                             String errString = "Cannot find value pairs for " + pairsName;
