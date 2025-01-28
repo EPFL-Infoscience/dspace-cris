7556c77bccd9808503218d997dc8ec71e9420f78
Daniele Ninfo <daniele.ninfo@4science.com>
2025-01-28 01:45:37 +0100

    CST-17561 fixed test to avoid contacting grobid directly

--- dspace-api/src/test/java/org/dspace/importer/external/service/ImportServiceTest.java
+++ dspace-api/src/test/java/org/dspace/importer/external/service/ImportServiceTest.java
@@ -7,15 +7,27 @@
  */
 package org.dspace.importer.external.service;
 
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.any;
+import static org.mockito.Mockito.anyString;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.when;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
 
 import org.dspace.AbstractDSpaceIntegrationTest;
 import org.dspace.importer.external.datamodel.ImportRecord;
+import org.dspace.importer.external.service.components.FileSource;
 import org.dspace.services.ConfigurationService;
 import org.dspace.services.factory.DSpaceServicesFactory;
+import org.dspace.submit.extraction.GrobidImportMetadataSourceServiceImpl;
 import org.dspace.utils.DSpace;
 import org.junit.Before;
 import org.junit.Test;
@@ -51,8 +63,22 @@ public class ImportServiceTest extends AbstractDSpaceIntegrationTest {
         maxFileLength = configurationService.getLongProperty("grobid.max-file-size-accepted", 10 * 1024 * 1024); // 10Mb
         try {
             configurationService.setProperty("grobid.max-file-size-accepted", 100 * 1024 * 1024L); // 100Mb
-            ImportRecord result = importService.getRecord(simpleArticle, SIMPLE_ARTICLE);
+
+            ImportRecord mockRecord = mock(ImportRecord.class);
+            FileSource fileSource = spy(new GrobidImportMetadataSourceServiceImpl());
+            when(fileSource.isValidSourceForFile(anyString())).thenReturn(true);
+            doReturn(mockRecord).when(fileSource).getRecord(any(InputStream.class));
+
+            ImportService spyImportService = spy(importService);
+            doReturn(fileSource).when(spyImportService).getFileSource(any(InputStream.class), anyString());
+
+            ImportRecord result = spyImportService.getRecord(simpleArticle, SIMPLE_ARTICLE);
             assertNotNull(result);
+
+            FileSource actualFileSource =
+                    importService.getFileSource(new FileInputStream(simpleArticle), SIMPLE_ARTICLE);
+            assertTrue(actualFileSource.isFileSizeAccepted(simpleArticle.length()));
+
         } finally {
             configurationService.setProperty("grobid.max-file-size-accepted", maxFileLength);
         }
@@ -65,6 +91,10 @@ public class ImportServiceTest extends AbstractDSpaceIntegrationTest {
             configurationService.setProperty("grobid.max-file-size-accepted", 1L); // 1b
             ImportRecord result = importService.getRecord(simpleArticle, SIMPLE_ARTICLE);
             assertNull(result);
+
+            FileSource actualFileSource =
+                    importService.getFileSource(new FileInputStream(simpleArticle), SIMPLE_ARTICLE);
+            assertFalse(actualFileSource.isFileSizeAccepted(simpleArticle.length()));
         } finally {
             configurationService.setProperty("grobid.max-file-size-accepted", maxFileLength);
         }
