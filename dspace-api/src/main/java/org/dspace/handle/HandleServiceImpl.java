2cf5ade20e5ec85983d5b685302a0870b3b89e28
Adamo <adamo.fapohunda@4science.com>
2025-03-25 10:02:16 +0100

    [CST-18212] Added method to retrieve handle by obj id.

--- dspace-api/src/main/java/org/dspace/handle/HandleServiceImpl.java
+++ dspace-api/src/main/java/org/dspace/handle/HandleServiceImpl.java
@@ -274,6 +274,17 @@ public class HandleServiceImpl implements HandleService {
     public String findHandle(Context context, DSpaceObject dso)
         throws SQLException {
         List<Handle> handles = dso.getHandles();
+        return getHandleString(handles);
+    }
+
+    @Override
+    public String findHandleByObjId(Context context, DSpaceObject dso)
+        throws SQLException {
+        List<Handle> handles = handleDAO.getHandlesByDSpaceObject(context, dso);
+        return getHandleString(handles);
+    }
+
+    private String getHandleString(List<Handle> handles) {
         if (CollectionUtils.isEmpty(handles)) {
             return null;
         } else {
