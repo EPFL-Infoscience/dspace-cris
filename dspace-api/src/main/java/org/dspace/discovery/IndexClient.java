4f571b2582d35cc1ae64bb7b5fec00b4eaf48b54
Andrea Bollini <andrea.bollini@4science.com>
2025-05-12 14:54:33 +0200

    CST-20334 remove -m option to index-discovery as metric are included in solr during the normal index process

--- dspace-api/src/main/java/org/dspace/discovery/IndexClient.java
+++ dspace-api/src/main/java/org/dspace/discovery/IndexClient.java
@@ -121,10 +121,6 @@ public class IndexClient extends DSpaceRunnable<IndexDiscoveryScriptConfiguratio
             }
         }
 
-        boolean metricUpdate = true;
-        if (commandLine.hasOption("m")) {
-            metricUpdate = false;
-        }
         if (indexClientOptions == IndexClientOptions.REMOVE) {
             handler.logInfo("Removing " + commandLine.getOptionValue("r") + " from Index");
             for (IndexableObject idxObj : indexableObjects.get()) {
@@ -173,25 +169,6 @@ public class IndexClient extends DSpaceRunnable<IndexDiscoveryScriptConfiguratio
         }
 
         handler.logInfo("Done with indexing");
-        if (metricUpdate) {
-            if (indexableObjects.isPresent()) {
-                final String param = indexClientOptions == IndexClientOptions.REMOVE ?
-                        commandLine.getOptionValue('r') :
-                        commandLine.getOptionValue('i');
-                UUID uuid = null;
-                try {
-                    uuid = UUID.fromString(param);
-                } catch (Exception e) {
-                    uuid = HandleServiceFactory.getInstance()
-                            .getHandleService().resolveToObject(context, param).getID();
-                }
-
-                updateCrisMetricsInSolrDocService.performUpdate(context, handler, true, uuid);
-
-            } else {
-                updateCrisMetricsInSolrDocService.performUpdate(context, handler, true);
-            }
-        }
     }
 
     @Override
